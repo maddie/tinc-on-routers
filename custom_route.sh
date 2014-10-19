e48e5842aff333ed53d2db1d3ed4576a6fc9854f
Maddie Zhan <maddie.zhan@cynovan.com>
2014-10-19 10:04:02 +0800

    Finally fixed the culprit of not loading routes on boot
    Somehow the find utility doesn't return values upon boot, so the 'for'
    loop is being skipped in its entirety, since there's nothing for it to
    iterate.
    
    Use the almost-guarantee-to-work kind of way to achieve the same result.

--- custom_route.sh
+++ custom_route.sh
@@ -8,27 +8,33 @@ INTERFACE=$1
 
 echo "$(ts): Begin custom route script." >> $LOGFILE
 
-for HOSTFILE in $(/usr/bin/find $HOSTDIR/ -type f); do
-  echo "$(ts): Loading hosts from '$HOSTFILE'..." >> $LOGFILE
-  for HOST in $(cat "$HOSTFILE" | grep -vE '^#'); do
-    ip route add $HOST via $GATEWAY dev $INTERFACE >> $LOGFILE 2>&1
-    if [ $? -ne 0 ]; then
-      echo "$(ts): Adding host '$HOST' from '$HOSTFILE' failed." >> $LOGFILE
-    fi
-  done
-  #echo "$(ts): Finished loading hosts from '$HOSTFILE'." >> $LOGFILE
+for HOSTFILE in $HOSTDIR/*; do
+  if [ -d $HOSTFILE ]; then
+    continue
+  else
+    echo "$(ts): Loading hosts from '$HOSTFILE'..." >> $LOGFILE
+    for HOST in $(grep -vE '^#' "$HOSTFILE"); do
+      /usr/sbin/ip route add $HOST via $GATEWAY dev $INTERFACE >> $LOGFILE 2>&1
+      if [ $? -ne 0 ]; then
+        echo "$(ts): Adding host '$HOST' from '$HOSTFILE' failed." >> $LOGFILE
+      fi
+    done
+  fi
 done
 echo "$(ts): Finished loading hosts." >> $LOGFILE
 
-for NETFILE in $(/usr/bin/find $NETDIR/ -type f); do
-  echo "$(ts): Loading subnets from '$NETFILE'..." >> $LOGFILE
-  for NET in $(cat "$NETFILE" | grep -vE '^#'); do
-    ip route add $NET via $GATEWAY dev $INTERFACE >> $LOGFILE 2>&1
-    if [ $? -ne 0 ]; then
-      echo "$(ts): Adding subnet '$NET' from '$NETFILE' failed." >> $LOGFILE
-    fi
-  done
-  #echo "$(ts): Finished loading subnets from '$NETFILE'." >> $LOGFILE
+for NETFILE in $NETDIR/*; do
+  if [ -d $HOSTFILE ]; then
+    continue
+  else
+    echo "$(ts): Loading subnets from '$NETFILE'..." >> $LOGFILE
+    for NET in $(grep -vE '^#' "$NETFILE"); do
+      /usr/sbin/ip route add $NET via $GATEWAY dev $INTERFACE >> $LOGFILE 2>&1
+      if [ $? -ne 0 ]; then
+        echo "$(ts): Adding subnet '$NET' from '$NETFILE' failed." >> $LOGFILE
+      fi
+    done
+  fi
 done
 echo "$(ts): Finished loading subnets." >> $LOGFILE
 
